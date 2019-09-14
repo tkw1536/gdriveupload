8d93c123b1dc34c0e01f8c5c6931d42407c1fb23
Tom Wiesing <tkw01536@gmail.com>
2019-09-14 10:43:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# gdrivebackup
+
+This is a quick tool to upload a specific file to a specific google drive folder and account. 
+Needs a service account with domain-wide delegation enabled. 
+
+Usage:
+
+```
+Usage of ./gdriveupload:
+  -credspath string
+        Path to credentials
+  -filepath string
+        Path of file to be uploaded
+  -folderid string
+        ID of folder to upload file to
+  -owner string
+        Owner to transfer file to
+```
+
+## License
+
+GPL3, see [gpl.txt](gpl.txt)
---
+++ gdriveupload.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"golang.org/x/oauth2/jwt"
+
+	"google.golang.org/api/drive/v3"
+)
+
+// Options for a run of gdrivebackup
+type Options struct {
+	CredsPath  string // Path to service account credentials
+	FilePath   string // Path to file that should be uploaded
+	FolderID   string // ID of folder to upload content in
+	OwnerEmail string // Email of user to set ownership for
+}
+
+func readOptions() (opts Options) {
+
+	// read the arguments from command line
+	flag.StringVar(&opts.CredsPath, "credspath", "", "Path to credentials")
+	flag.StringVar(&opts.FilePath, "filepath", "", "Path of file to be uploaded")
+	flag.StringVar(&opts.FolderID, "folderid", "", "ID of folder to upload file to")
+	flag.StringVar(&opts.OwnerEmail, "owner", "", "Owner to transfer file to")
+	flag.Parse()
+
+	// check that there aren't any extra arguments
+	if flag.NArg() != 0 {
+		fmt.Fprintln(os.Stderr, "Too many arguments.")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	// check that credspath exists
+	if _, err := os.Stat(opts.CredsPath); err != nil || opts.CredsPath == "" {
+		fmt.Fprintf(os.Stderr, "credspath %q does not exist.\n", opts.CredsPath)
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	// check that filepath exists
+	if _, err := os.Stat(opts.FilePath); err != nil || opts.FilePath == "" {
+		fmt.Fprintf(os.Stderr, "filepath %q does not exist.\n", opts.FilePath)
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	if opts.FolderID == "" {
+		fmt.Fprintln(os.Stderr, "FolderID must not be empty.")
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	return
+}
+
+func readCredentials(opts Options) (service *drive.Service, err error) {
+	bytes, err := ioutil.ReadFile(opts.CredsPath)
+	if err != nil {
+		return
+	}
+
+	var config *jwt.Config
+	if config, err = google.JWTConfigFromJSON(bytes, drive.DriveScope); err != nil {
+		return
+	}
+	config.Subject = opts.OwnerEmail
+
+	client := config.Client(oauth2.NoContext)
+	return drive.New(client)
+}
+
+func uploadFile(opts Options, service *drive.Service) (err error) {
+	content, err := os.Open(opts.FilePath)
+	if err != nil {
+		return err
+	}
+
+	// prepare the filename
+	_, name := filepath.Split(opts.FilePath)
+	f := &drive.File{
+		Name:    name,
+		Parents: []string{opts.FolderID},
+	}
+
+	// create the file
+	_, err = service.Files.Create(f).Media(content).Do()
+	return err
+}
+
+func main() {
+	// read in the options from command line
+	opts := readOptions()
+
+	// generate credentials
+	creds, err := readCredentials(opts)
+	if err != nil {
+		panic(err)
+	}
+
+	err = uploadFile(opts, creds)
+	if err != nil {
+		panic(err)
+	}
+}
